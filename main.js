c52d538796f2cd5b98ea179ba402e83f925e36d0
vitordanielp <vitordaniel97.vtd@gmail.com>
2024-08-03 19:03:07 -0300

    contador funcional

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-let segundos = 60;
-
+const dataCasamento = '2024-09-28T16:00:00';
+const contadorDias = document.querySelector("#contador-dias");
+const contadorHoras = document.querySelector("#contador-horas");
+const contadorMinutos = document.querySelector("#contador-minutos");
 const contadorSegundos = document.querySelector("#contador-segundos");
-function atualizaTempo(valor) {
-    contadorSegundos.textContent = valor;
-    segundos -=1;
+
+function updateCountdown() {
+    const targetDate = new Date(dataCasamento);
+    const now = new Date();
+    const timeDiff = targetDate - now;
+
+    const diasRestantes = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const horasRestantes = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosRestantes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const segundosRestantes = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    contadorDias.textContent = diasRestantes;
+    contadorHoras.textContent = horasRestantes;
+    contadorMinutos.textContent = minutosRestantes;
+    contadorSegundos.textContent = segundosRestantes;
+
 }
 
-
-setInterval(() => {
-    if (segundos >= 0) {
-        atualizaTempo(segundos);
-    }
-    else {
-        segundos = 60;
-        atualizaTempo(segundos);
-    }
-}, 1000);
+setInterval(updateCountdown, 1000);
+updateCountdown();
